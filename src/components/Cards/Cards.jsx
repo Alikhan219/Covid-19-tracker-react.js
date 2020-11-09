de00c4ac64ec3cc3a72efe3e64913f09e11dd7a0
Alikhan219 <merabhai786786@gmail.com>
2020-11-09 11:34:39 -0800

    importing the components to App.js file

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,8 @@
+importt React from "react"
+
+const Cards=()=>{
+    return(
+        <h1>Cards</h1>
+    )
+}
+export default Cards;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,8 @@
+importt React from "react"
+
+const Charts=()=>{
+    return(
+        <h1>Charts</h1>
+    )
+}
+export default Charts;
