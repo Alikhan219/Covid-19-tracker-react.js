5a4472e69a4d9b7a7d529774bcd81920ca378b95
Alikhan219 <merabhai786786@gmail.com>
2020-11-10 12:16:16 -0800

    working on chart data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import { fetchDailyData, fetchData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
-const Charts=()=>{
-    return(
-        <h1>Charts</h1>
-    )
-}
+const Charts = () => {
+  const [dailyData, setDailyData] = useState({});
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  },[]);
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: "",
+        datasets: [{}, {}],
+      }}
+    />
+  ) : null;
+
+  return <h1>Charts</h1>;
+};
 export default Charts;
