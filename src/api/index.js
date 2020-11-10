ebb7e2ede89b8c7382ebc344162b72594eb4af52
Alikhan219 <merabhai786786@gmail.com>
2020-11-10 13:07:31 -0800

    complete the line chart and styling

--- src/api/index.js
+++ src/api/index.js
@@ -24,10 +24,12 @@ export const fetchDailyData= async ()=>{
   try{
          const {data}= await axios.get(`${url}/daily`)
          
-         const modifiedData= data.map((dailyData)=>{
-
-         })
-
+         const modifiedData= data.map((dailyData)=>({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+         }))
+        return modifiedData;
   } catch(error){
 
   }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -15,14 +15,14 @@ const Charts = () => {
   const lineChart = dailyData.length!==0 ? (
     <Line
       data={{
-        labels: dailyData(({date})=> date),
+        labels: dailyData.map(({date})=> date),
         datasets: [{
-            data: dailyData(({confirmed})=> confirmed),
+            data: dailyData.map(({confirmed})=> confirmed),
             label:'Infected',
             borderColor: "#3333ff",
             fill: true,
         }, {
-            data: dailyData(({deaths})=> deaths),
+            data: dailyData.map(({deaths})=> deaths),
             label:'Deaths',
             borderColor: "rgba(255,0,0,0.5)",
             fill: true,
