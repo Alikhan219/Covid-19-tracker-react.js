033f501c4ca079e50f0ef216b34763127929c13f
Alikhan219 <merabhai786786@gmail.com>
2020-11-10 10:23:18 -0800

    creating the cards

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
@@ -5,13 +6,13 @@ const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
   try {
     const {
-      data: { confirmed, recovered, deaths, lastUpdate },
+      data: { confirmed, recovered, deaths, lastUpdate }
     } = await axios.get(url);
     return {
         confirmed ,
         recovered,
         deaths,
-        lastUpdate,
+        lastUpdate
       };
   } catch (error) {}
 };
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,50 @@
-import React from "react"
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from './Cards.modules.css'
 
-const Cards=(props)=>{
-    console.log(props)
-    return(
-        <h1>Cards</h1>
-    )
-}
+const Cards = (props) => {
+  console.log(props);
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography varaint="h5">REAL DATA </Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography varaint="h5">REAL DATA </Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography varaint="h5">REAL DATA </Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of deaths caused by COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 export default Cards;
